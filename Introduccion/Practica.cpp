d24ddbed02fb0340adb04641b46c74c093e66d7e
franxga <francisco.palomar35@my.unitec.edu.mx>
2024-10-07 12:42:11 -0600

    Create Practica.cpp

---
+++ Introduccion/Practica.cpp
@@ -0,0 +1,26 @@
+// Hola Munda.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+
+#include<iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	/* 1.1 Declare una variable tipo int que tenga el número de materias que estás
+	cursando actualmente. */
+	int numeroMaterias = 5;
+	cout << "1.1 Numero de materias que estoy cursando: " << numeroMaterias << endl;
+
+	/*1.2Declare una variable tipo int que guarde el valor del cuatrimestre que
+estas cursando.*/
+	int valorCuatrimestre = 6;
+	cout << "1.2 Valor del cuatrimestre que estas cursando: " << valorCuatrimestre << endl;
+
+	/*Declara una variable tipo
+string con identificador “materia”, e inicializar con el nombre de esta materia.*/
+	string materia = ""; 
+		cout << 
+}
