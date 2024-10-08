a4a0bca26d130fe24de2fe172014371823d1eb5d
franxga <francisco.palomar35@my.unitec.edu.mx>
2024-10-08 11:14:04 -0600

    Update Tarea 1.cpp

--- Introduccion/Tarea 1.cpp
+++ Introduccion/Tarea 1.cpp
@@ -47,64 +47,160 @@ string con identificador “materia”, e inicializar con el nombre de esta mate
 
 	/*Declara una variable de tipo string con identificador “apellidoMaterno”, e inicializar con tu apellido materno. */
 	string apellidoMaterno = "Garcia ";
-		cout << "1.7 Apellido Materno: " << apellidoMaterno << endl;
+	cout << "1.7 Apellido Materno: " << apellidoMaterno << endl;
 
 	/*Declara una variable tipo long con identificador “celular”, e inicializar con tu número celular. */
-		long long int celular = 5543711698l;
-		cout << "1.8 Numero de Celular: " << celular << endl;
+	long long int celular = 5543711698l;
+	cout << "1.8 Numero de Celular: " << celular << endl;
 
 	/*Declara una variable tipo long con identificador “casa”, e inicializar con tu número de casa. */
-		long int casa = 58228417l;
-		cout << "1.9 Numero de casa: " << casa << endl;
+	long int casa = 58228417l;
+	cout << "1.9 Numero de casa: " << casa << endl;
+
+	/*Declara una variable tipo string con identificador “correoQueMasUso”, e inicializar con el correo electrónico que más uses. */
+	string correoQueMasUso = "gaelpalomar1@gmail.com ";
+	cout << "1.10 Correo Electronico Personal: " << correoQueMasUso << endl;
+
+	/*Declara una variable tipo string con identificador “correMyUnitec”, e inicializar con el correo electrónico que tienes asignado en el Unitec. */
+	string correoMyUnitec = "francisco.palomar35@my.unitec.edu.mx ";
+	cout << "1.11 Correo Unitec: " << correoMyUnitec << endl;
+
+	/*Declara una variable tipo bool con identificador “blackboard”, e inicializar con valor true, si lo has usado anteriormente, de lo contrario inicializar con valor false. */
+	bool blackboard = true;
+	cout << "1.12 ¿He usado Blackboard antes? " << (blackboard ? "Si" : "No") << endl;
+
+	//Ejercicios del Módulo : Tipo de Variables.
+
+	string ejercios = "Tipo de Variable ";
+	cout << "Ejercicio del Modulo: " << ejercios << endl;
+
+	string ejercicios02 = "Analisis de errores de sintaxis ";
+	cout << "Seccion: " << ejercicios02 << endl;
+
+	// Sección : Análisis de errores de sintaxis.
+
+	// Ejercicio 1 
+	// int 1numero = 5; 
+
+	int numero1 = 5;
+	cout << "Ejercicio 1: " << numero1 << endl;
+
+	// Ejercicio 2 
+	// float valor@ = 3.14; 
+
+	float valor = 3.14;
+	cout << "Ejercicio 2: " << valor << endl;
+
+	// Ejercicio 3 
+	// double double = 9.81; 
+
+	double gravedad = 9.81;
+	cout << "Ejercicio 3: " << gravedad << endl;
+
+	// Ejercicio 4
+	// char letra = 'AB'; 
+
+	char letra = 'A';
+	cout << "Ejercicio 4: " << letra << endl;
+
+	// Ejercicio 5
+	// bool esverdadero = "true";
+
+	bool verdadero = true;
+	cout << "Ejercicio 5 Verdadero? " << (verdadero ? "Si" : "No") << endl;
+
+	//Ejercicio 6
+	// int numero = 10.5
+
+	int numero = 10;
+	cout << "Ejercicio 6: " << numero << endl;
+
+	// Ejercicio 7
+	// string nombre = 'Juan';
+
+	string nombreJ = "Juan";
+	cout << "Ejercicio 7: " << nombreJ << endl;
+
+	// Ejercicio 8 
+	// constant int PI = 3.14
+
+	const float pi = 3.14;
+		cout << fixed;
+		cout << setprecision(2);
+		cout << "Ejercicio 8: " << pi << endl;
+
+		// Ejercicio 9
+		// int suma = 5 +;
+
+		int suma = 5 + 2;
+		cout << "Ejercicio 9: " << suma << endl;
+
+		// Ejercicio 10
+		// int numero;
+		// numero 10
 		
-		/*Declara una variable tipo string con identificador “correoQueMasUso”, e inicializar con el correo electrónico que más uses. */
-		string correoQueMasUso = "gaelpalomar1@gmail.com ";
-		cout << "1.10 Correo Electronico Personal: " << correoQueMasUso << endl;
-
-		/*Declara una variable tipo string con identificador “correMyUnitec”, e inicializar con el correo electrónico que tienes asignado en el Unitec. */
-		string correoMyUnitec = "francisco.palomar35@my.unitec.edu.mx ";
-		cout << "1.11 Correo Unitec: " << correoMyUnitec << endl;
-
-		/*Declara una variable tipo bool con identificador “blackboard”, e inicializar con valor true, si lo has usado anteriormente, de lo contrario inicializar con valor false. */
-		bool blackboard = true; 
-		cout << "1.12 ¿He usado Blackboard antes? " << (blackboard ? "Si" : "No") << endl;
-
-		//Ejercicios del Módulo : Tipo de Variables.
+		int numeroDiez = 10;
+		cout << "Ejercicio 10: " << numero << endl;
 		
-		string ejercios = "Tipo de Variable ";
-		cout << "Ejercicio del Modulo: " << ejercios << endl;
+		// Ejercicio 11
+		// int numero = "diez"
 
-		string ejercicios02 = "Analisis de errores de sintaxis ";
-		cout << "Seccion: " << ejercicios02 << endl; 
-			 
-		// Sección : Análisis de errores de sintaxis.
+		int numero10 = 10;
+		cout << "Ejercicio 11: " << numero10 << endl;
 
-		// Ejercicio 1 
-		// int 1numero = 5; 
+		// Ejercicio 12
+		// float altura = 1,75;
 
-		int numero1 = 5;
-		cout << "Ejercicio 1: " << numero1 << endl;
+		float altura = 1.75;
+		cout << "Ejercicio 12: " << altura << endl;
+		
+		// Ejercicio 13 = 
+		// char inicial = "J"
 
-		// Ejercicio 2 
-		// float valor@ = 3.14; 
+		char inicial = 'J';
+		cout << "Ejercicio 13: " << inicial << endl;
 
-		float valor = 3.14;
-		cout << "Ejercicio 2: " << valor << endl;
+		// Ejercicio 14
+		// bool esMayor = 1;
 
-		// Ejercicio 3 
-		// double double = 9.81; 
+		bool esMayor = true;
+		cout << "Ejecicio 14 es Mayor : " << (esMayor ? "1" : "2") << endl;
+		
+		// Ejercicio 15
+		// string apellido = Juan; 
 
-		double gravedad = 9.81;
-		cout << "Ejercicio 3: " << gravedad << endl;
+		string apellido = "Juan";
+		cout << "Ejercicio 15: " << apellido << endl;
 
-		// Ejercicio 4
-		// char letra = 'AB'; 
+		// Ejercicio 16
+		// int resultado = 5 * ;
 
-		char letra = 'A';
-		cout << "Ejercicio 4: " << letra << endl;
+		int resultado = 5 * 8;
+		cout << "Ejercicio 16: " << resultado << endl;
+
+		// Ejecicio 17 
+		// double area = area 3.14 * radio^2;
 
 
 
-		return 0;
+		// Ejercicio 18
+		// int edad = 25
+
+		int edad = 25;
+		cout << "Ejecicio 18: " << edad << endl;
+
+		// Ejercicio 19
+		// float peso = 70.5f;
+
+		float peso = 70.5;
+		cout << "Ejercicio 19: " << peso << endl;
+
+		// Ejercicio 20 
+		// int suma = 10 + 20
+
+		int sumas = 10 + 20;
+		cout << "Ejercicio 20: " << sumas << endl;
+
+	return 0; 
 }
 
