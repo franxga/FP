78a2048a33defcf0263b09e77513263df246e3db
franxga <francisco.palomar35@my.unitec.edu.mx>
2024-10-21 12:22:19 -0600

    Update 21_Oct.cpp

--- Introduccion/21_Oct.cpp
+++ Introduccion/21_Oct.cpp
@@ -69,18 +69,49 @@ if (temperatura > 17)
 
 /*5. Preguntar si al usuario si está a favor de la paz, guardar en una variable tipo char llamada voto, validar 
   si el voto es ‘S’ o el voto es ‘N’ escribir en pantalla “voto valido” en caso de que sea cualquier otro carácter escribir, “voto invalido” */
+char voto; 's';
+cout << "A favor s/n: ";
+cin >> voto;
+if (voto == 's' or voto == 'n' or voto == 'S' or voto == 'N')
+{
+	cout
+		<< "Voto validado "
+		<< endl;
+	}
+else
+{
+	cout
+		<< "Voto invalido"
+		<< endl;
+}
 
 /* 6. Preguntar al usuario cuantas horas ha trabajado el empleado durante la semana, si, el empleado trabajo más de 40 horas, 
 el excedente de horas se multiplicará por 75 pesos y publicar este cálculo en pantalla que será el bono del empleado.*/
+int horasTrabajadas = 0;
+cout << "Horas trabajadas: ";
+cin >> horasTrabajadas;
+	cout << endl;
+
+int horasBono = horasTrabajadas - 40;
+
+if (horasTrabajadas > 0)
+{
+	horasBono = horasBono * 75;
+	cout << "Bono " << horasBono << endl;
+}
 
 /* 7. Crear dos variables de tipo int, que guarden el día y mes, del cumpleaños del usuario. Si la fecha corresponde a mes 9 y día 15, escribir en pantalla, “Feliz cumpleaños”*/
 
+	
 /* 8. Crear una variable que se llame velocidad, pregunta por este valor, si esta es negativa, escribir en pantalla “velocidad negativa”, si la velocidad es cero, escribir en pantalla “en reposo”, 
 y si es positiva escribir en pantalla “velocidad positiva”.*/
 
+	
 /* 9.Crear las siguientes variables de tipo int: huevos, mantequilla, leche, harina, levadura, azucar y sal. Asigna valores aleatorios a cada variable, entre 1 y 5. 
 Si todos los ingredientes tienen al menos un elemento, escribir en pantalla “Puedes crear hot cakes” de lo contrario escribir en pantalla “No hay suficientes ingredientes” */
 
+	
 /* 10. Crear un programa en C++ que determine cuantos hot cakes se pueden hacer basado en la pregunta anterior.*/
 
+	
 }
