efef9789652ecd42ff42c664d76b474d467730a8
franxga <francisco.palomar35@my.unitec.edu.mx>
2024-10-21 12:38:47 -0600

    Create Practica_C.cpp

---
+++ Introduccion/Practica_C.cpp
@@ -0,0 +1,37 @@
+// 21 Oct.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+/*1.1 Escriba el código que pregunte al usuario por un número y escribir el resultado del
+cuadrado. De la siguiente manera:
+
+Ingrese un número: 5
+El cuadrado de 5 es 25
+
+*/
+	float numero, potencia;
+
+	cout << "Ingresa un numero: ";
+	cin >> numero;
+
+	potencia = numero * numero;
+
+	cout << "La potencia de  " << numero << " es: " << potencia << endl;
+
+/*1.2 Escriba un código que solicite un número entero e imprima los resultados de x, 2x, 3x,
+4x y 5x. Donde x es el número ingresado por el usuario, y los subsecuentes implican
+multiplicar por 2, multiplicar por 3, multiplicar por 4 y multiplicar por 5, y escribir el
+resultado en el siguiente formato:
+
+Ingrese un número: 7
+7 -- 14 -- 21 -- 28 -- 35
+*/
+
+
+
+}
